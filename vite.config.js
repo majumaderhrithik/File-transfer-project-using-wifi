8d18a6cdfab008bed07a31ea7c66679631f5b129
majumaderhrithik <hrithikmajumader@gmail.com>
2026-04-06 22:42:36 +0600

    Remove failing GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: '/File-transfer-project-using-wifi/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
